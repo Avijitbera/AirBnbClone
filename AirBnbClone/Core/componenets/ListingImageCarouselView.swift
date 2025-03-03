a0674f27f50ef4bedbd5d2bb46fdeea61186cf21
Avijitbera <avijitbera775@gmail.com>
2025-03-03 19:35:11 +0530

    created listing details page

---
+++ AirBnbClone/Core/componenets/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  AirBnbClone
+//
+//  Created by Avijit bera on 01/03/25.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
