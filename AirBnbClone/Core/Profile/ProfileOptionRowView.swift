90eacbdb50016eb105573a1f7be5372e9a7574ca
Avijitbera <avijitbera775@gmail.com>
2025-03-14 09:54:44 +0530

    completed thw listing data models

---
+++ AirBnbClone/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionRowView.swift
+//  AirBnbClone
+//
+//  Created by Avijit bera on 10/03/25.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionRowView()
+}
